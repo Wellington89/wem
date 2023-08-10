1f73f7bfa6766cbb35373468335d017096f0b7e1
Wellington Rossato <86727407+Wellington89@users.noreply.github.com>
2023-08-10 18:47:04 -0300

    atualização 3.4

--- sistema/CRM/sistema.js
+++ sistema/CRM/sistema.js
@@ -14,3 +14,58 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.classList.add("active");
 }
+
+//-----------------------------------CALCULADORA DE ROI-----------------------------------//
+function calcularROI() {
+  const orcamento = parseFloat(document.getElementById("orcamento").value);
+  const cpc = parseFloat(document.getElementById("cpc").value);
+  const taxaConversao = parseFloat(
+    document.getElementById("taxa-conversao").value
+  );
+  const ticketMedio = parseFloat(document.getElementById("ticket-medio").value);
+  const taxaLeadVendas = parseFloat(
+    document.getElementById("taxa-lead-vendas").value
+  );
+
+  const numeroCliques = orcamento / cpc;
+  const numeroLeads = numeroCliques * (taxaConversao / 100);
+  const custoPorLead = orcamento / numeroLeads;
+  const receitaEsperada = numeroLeads * (taxaLeadVendas / 100) * ticketMedio;
+  const lucroEsperado = receitaEsperada - orcamento;
+  const roi = (lucroEsperado / orcamento) * 100;
+
+  const resultadoElement = document.getElementById("resultado");
+  resultadoElement.innerHTML = `
+    Número de Cliques: ${numeroCliques.toFixed(0)}<br>
+    Número de Leads: ${numeroLeads.toFixed(0)}<br>
+    Custo por Lead: R$ ${custoPorLead.toFixed(2)}<br>
+    Receita Esperada: R$ ${receitaEsperada.toFixed(2)}<br>
+    Lucro Esperado: R$ ${lucroEsperado.toFixed(2)}<br>
+    Retorno do Investimento em Anúncios: ${roi.toFixed(2)}%
+  `;
+}
+function limparDados() {
+  document.getElementById("orcamento").value = "";
+  document.getElementById("cpc").value = "";
+  document.getElementById("taxa-conversao").value = "";
+  document.getElementById("ticket-medio").value = "";
+  document.getElementById("taxa-lead-vendas").value = "";
+  document.getElementById("resultado").innerHTML = "";
+}
+
+
+
+
+
+
+
+
+
+
+  //-----------------------------------CALCULADORA DE ROI-----------------------------------//
+  
+  
+  
+  
+  
+
